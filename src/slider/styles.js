6ac1092bb35b0edfc415646e34ce2c861e53d664
Romario Herbert <romarioh39@gmail.com>
2018-07-21 12:01:34 -0600

    updates on slider

--- src/slider/styles.js
+++ src/slider/styles.js
@@ -2,9 +2,22 @@ import styled from 'react-emotion'
 
 export const Container = styled('div')`
 display: flex;
-flex-direction: column;
+flex-direction: row;
 justify-content: center; 
 background-color: white;
-background: url("./homepage1.jpg") no-repeat center center fixed; 
 background-size: contain;
+`
+export const Pix = styled('div')`
+width: 300px;
+height: 200px;
+border: 5px solid white;
+box-shadow: 10px 10px 10px #888888;
+margin-right: 7px;
+`
+export const Spic = styled('p')`
+width: 300px;
+height: 200px;
+border: 5px solid white;
+box-shadow: 10px 10px 10px #888888;
+margin-right: 7px;
 `
--- src/styles.js
+++ src/styles.js
@@ -2,9 +2,20 @@ import styled from 'react-emotion'
 
 export const Container = styled('div')`
 display: flex;
-flex-direction: column;
-justify-content: center;
-font-family: Times;
-font-size: 20%; 
-color: yellow;
+flex-direction: row;
+justify-content: center; 
+background-color: white;
+background-size: contain;
+`
+export const Pix = styled('div')`
+display: felx;
+width: 300px;
+height: 200px;
+border: 5px solid white;
+box-shadow: 10px 10px 10px #888888;
+margin-right: 7px;
+`
+export const Spic = styled('div')`
+margin-bottom: 200px;
+box-shadow: 6px 0px 0px 6px;
 `
